4a815ee0070b6a0ad02c90075ee3841e20d7b6ae
anujkumar123-4 <anujkumar6206945227@gmail.com>
2024-06-13 08:40:32 +0530

    Angular_Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {LoginComponent } from './login/login.component'
+import {HeaderComponent } from './header/header.component'
+import {LandingPageComponent } from './landing-page/landing-page.component'
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,LoginComponent,HeaderComponent,LandingPageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'my-project';
+  show=false
+}
